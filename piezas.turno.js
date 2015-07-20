e82fbbecc7a60fbb70d42ccc3fa138e506b23b91
Aldo Mendez <aldomendez86@gmail.com>
2015-07-20 18:41:43 -0500

    createSOD test completed

--- piezas.turno.js
+++ piezas.turno.js
@@ -8,31 +8,36 @@
   var time = new Date();
   // time = new Date(time.getFullYear(),time.getMonth(),time.getDate(),14,59);
   var generateTimeinHHMI = function(time){
+    if(time == null){
+      throw new Error('You should pass a Date object for this to work')
+    }
     return function(){
       return time.getHours() + pad(time.getMinutes())
     } // regresa la hora en formato HHMI
   }
   hr = generateTimeinHHMI(new Date())
+    
   var normalizedTarget = function(start,end,inc){
     return Math.round(((end - start)/1000/60/60) * inc);
   }
-  createSOD = function(dayOffset,h,m){
+  createSOD = function(date, dayOffset, h, m){
     // SOD = start of day;
     // Crea la base de hora para la que se estara haciendo la verificacion.
-    return new Date(time.getFullYear(),time.getMonth(),time.getDate() + dayOffset,h,m)
+    return new Date(date.getFullYear(),date.getMonth(),date.getDate() + dayOffset,h,m)
   }
   target = function (numberOfMachines){
+    var date = new Date()
     if(hr() > 630 && hr() < 1500){
-      sod= createSOD(0,6,30);
+      sod= createSOD(date,0,6,30);
       return normalizedTarget(sod, time, numberOfMachines * 11)
     } else if(hr()>1500 && hr()<2300){
-      sod= createSOD(0,15,0)
+      sod= createSOD(date,0,15,0)
       return normalizedTarget(sod, time, numberOfMachines * 11)
     } else if (hr()>2300){
-      sod= createSOD(0,23,0)
+      sod= createSOD(date,0,23,0)
       return normalizedTarget(sod, time, numberOfMachines * 11)
     } else if (hr() < 630){
-      sod= createSOD(-1,23,0)
+      sod= createSOD(date,-1,23,0)
       return normalizedTarget(sod, time, numberOfMachines * 11)
     }
   }
--- spec/Spec.piezas.turno.js
+++ spec/Spec.piezas.turno.js
@@ -15,13 +15,16 @@ describe('helper methods', function(){
 describe('generateTimeinHHMI ', function(){
 	it('is s closure', function(){
 		expect(typeof generateTimeinHHMI).toBe('function');
-		var hr = generateTimeinHHMI()
+		var hr = generateTimeinHHMI(new Date())
 		expect(typeof hr).toBe('function')
+		expect(hr()).toMatch(/\d{3,4}/)
 	});
+	it('should throw if called with out arguments', function(){
+		expect(generateTimeinHHMI).toThrow()
+	})
 	it('closure should return a string when called', function(){
 		hr = generateTimeinHHMI(new Date(1437428950496))
 		expect(hr()).toBe('1649')
-		expect(hr()).toMatch(/\d{3,4}/)
 		expect(typeof hr()).toBe('string')
 		
 	})
@@ -34,5 +37,36 @@ describe('generateTimeinHHMI ', function(){
 });
 
 describe('normalizedTarget', function(){
-	it('should')
+	it('should be a function', function(){
+		expect(typeof normalizedTarget).toBe('function')
+	})
+	it('should take two dates and an increment and return a number', function(){
+		var start = new Date(2015,7,20,15,0,0);
+		var end = new Date(2015,7,20,18,8,25);
+		expect( typeof normalizedTarget(start, end, 11) ).toBe('number')
+	})
+	it('should correctly calculates target of devices')
 });
+
+describe('createSOD', function(){
+	it('should return a date object', function(){
+		expect(typeof createSOD(new Date(), 0, 6,30)).toBe('object')
+	})
+	it('should calculate dates correctly', function(){
+		date = createSOD(new Date(), 0, 6,30)
+		expect( date.getHours() ).toBe(6)
+		expect( date.getMinutes() ).toBe(30)
+		
+		date = createSOD(new Date(2015,7,20), -1, 6,30)
+		expect( date.getDate() ).toBe(19)
+		expect( date.getHours() ).toBe(6)
+		expect( date.getMinutes() ).toBe(30)
+		
+		date = createSOD(new Date(2015,4,7), 0, 6,30)
+		expect( date.getFullYear() ).toBe(2015)
+		expect( date.getMonth() ).toBe(4)
+		expect( date.getDate() ).toBe(7)
+		expect( date.getHours() ).toBe(6)
+		expect( date.getMinutes() ).toBe(30)
+	})
+})
